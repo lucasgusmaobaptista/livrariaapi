e553d0870a89c07717f26981049272fd30aed2a3
Lucas <lucasgusmaobaptista@gmail.com>
2025-07-23 16:00:21 -0300

    adding new repository queries

--- src/main/java/me/lucasgusmao/livraria_api/repository/LivroRepository.java
+++ src/main/java/me/lucasgusmao/livraria_api/repository/LivroRepository.java
@@ -1,11 +1,15 @@
 package me.lucasgusmao.livraria_api.repository;
 
+import me.lucasgusmao.livraria_api.model.Autor;
 import me.lucasgusmao.livraria_api.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+    List<Livro> findByAutor(Autor autor);
 }
