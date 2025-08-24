cabf10fc090cc8fd2bbbd72b265aa959945ebc58
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-24 17:36:43 -0300

    creating user entities

---
+++ src/main/java/me/lucasgusmao/livraria_api/model/Usuario.java
@@ -0,0 +1,29 @@
+package me.lucasgusmao.livraria_api.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
