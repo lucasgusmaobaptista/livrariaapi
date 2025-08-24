6ca2136338058e697f45f7db8ef116062efa72c4
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-24 17:37:02 -0300

    creating UsuarioRepository

---
+++ src/main/java/me/lucasgusmao/livraria_api/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.livraria_api.repository;
+
+import me.lucasgusmao.livraria_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Usuario findByLogin(String login);
+}
