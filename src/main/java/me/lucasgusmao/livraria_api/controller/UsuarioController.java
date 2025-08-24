5d7567efe36504ec717025d52dc14e387f2482d6
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-24 17:37:22 -0300

    creating and coding UsuarioService

---
+++ src/main/java/me/lucasgusmao/livraria_api/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+package me.lucasgusmao.livraria_api.controller;
+import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.livraria_api.controller.dto.UsuarioDTO;
+import me.lucasgusmao.livraria_api.controller.mappers.UsuarioMapper;
+import me.lucasgusmao.livraria_api.model.Usuario;
+import me.lucasgusmao.livraria_api.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService service;
+    private final UsuarioMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody UsuarioDTO dto) {
+        var usuario = mapper.toEntity(dto);
+        /* if (usuario.getPassword() == null || usuario.getPassword().isBlank()) {
+            throw new IllegalArgumentException("A senha é obrigatória.");
+        } */
+        service.salvar(usuario);
+        return ResponseEntity.status(201).build();
+    }
+}
---
+++ src/main/java/me/lucasgusmao/livraria_api/service/UsuarioService.java
@@ -0,0 +1,25 @@
+package me.lucasgusmao.livraria_api.service;
+
+import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.livraria_api.model.Usuario;
+import me.lucasgusmao.livraria_api.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+    private final PasswordEncoder encoder;
+
+    public void salvar(Usuario usuario) {
+        var senha = usuario.getPassword();
+        usuario.setPassword(encoder.encode(senha));
+        repository.save(usuario);
+    }
+
+    public Usuario obterPorLogin(String login) {
+        return repository.findByLogin(login);
+    }
+}
