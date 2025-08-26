742b0728a72d4da0330840d2407c886b1815009d
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-26 18:59:24 -0300

    Create ClientRepository

---
+++ src/main/java/me/lucasgusmao/livraria_api/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package me.lucasgusmao.livraria_api.repository;
+
+import me.lucasgusmao.livraria_api.model.Client;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, UUID> {
+}
