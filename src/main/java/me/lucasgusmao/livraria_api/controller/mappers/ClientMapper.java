28b484e9663650d4cfc5de33f72d2ba1b94de874
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-26 19:08:16 -0300

    Create ClientDTO and ClientMapper

--- src/main/java/me/lucasgusmao/livraria_api/controller/mappers/ClientMapper.java
+++ src/main/java/me/lucasgusmao/livraria_api/controller/mappers/ClientMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
 
-    Client toCDTO(ClientDTO clientDTO);
+    Client toDTO(ClientDTO clientDTO);
 
     ClientDTO toEntity(Client client);
 }
