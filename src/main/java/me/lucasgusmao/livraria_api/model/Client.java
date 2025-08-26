436d907022185448aaac42103593cf3413058c3f
Lucas <lucasgusmaobaptista@gmail.com>
2025-08-26 19:22:24 -0300

    Add @Column to fix entity mapping errors

--- src/main/java/me/lucasgusmao/livraria_api/model/Client.java
+++ src/main/java/me/lucasgusmao/livraria_api/model/Client.java
@@ -14,11 +14,15 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "client_id", nullable = false)
     private String clientId;
 
+    @Column(name = "client_secret", nullable = false)
     private String clientSecret;
 
+    @Column(name = "redirect_uri", nullable = false)
     private String redirectURI;
 
+    @Column(name = "scope", nullable = false)
     private String scope;
 }
